ba13fc95ea01d9def49d490dcc457e3b558decdd
Himanshu Kumar <114076786+Hrajz@users.noreply.github.com>
2022-09-25 12:08:37 +0530

    Create Playing_with_character.c

---
+++ Assignment 2/Playing_with_character.c
@@ -0,0 +1,16 @@
+https://www.hackerrank.com/challenges/playing-with-characters/problem?isFullScreen=true
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    char ch[10],s[70],sen[70];
+    scanf("%s\n%s\n", ch,s);
+    gets(sen);
+    printf("%s\n%s\n%s", ch,s,sen);   
+    return 0;
+}

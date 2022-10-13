8202e59ce64ac26baf6bbcd2a27d7c593f9b9dd1
Himanshu Kumar <114076786+Hrajz@users.noreply.github.com>
2022-10-13 10:28:34 +0530

    Create Simple array sum.cpp

---
+++ Assignment 3 warm-up/Simple array sum.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+#include<vector>
+
+using namespace std;
+
+int main(){
+
+    int n,sum=0;
+
+    cin>>n;
+
+    vector<int>ar(n);
+
+    for(int i=0;i<n;i++)
+
+    {
+
+        cin>>ar[i];
+
+        sum+=ar[i];
+
+    }
+
+    cout<<sum;
+
+}
